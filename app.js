db82ef50fac20a86136b014aad717a942ceac34d
gyansingh chauhan <gyansinghchauhan85@gmail.com>
2026-01-05 17:49:20 +0530

    sign up and log in feature implemented successfully

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const mongo_url="mongodb://127.0.0.1:27017/velora";
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
 const session=require("express-session");
+const flash=require("connect-flash")
+const listingRouter=require("./routes/listing.js")
+const passport=require("passport");
+const localStrategy=require("passport-local");
+const User=require("./models/user.js");
+const userRouter=require("./routes/user.js")
+const loginRouter=require("./routes/login.js")
+const logoutRouter=require("./routes/logout.js")
+
+//Connecting to DataBase    
+
+
 
 main().then(()=>{
     console.log("Connected to DB")
@@ -32,10 +44,40 @@ const sessionOptions={
     saveUninitialized:true,
 }
 app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize())
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next)=>{
+    res.locals.success=req.flash("success")
+    res.locals.error=req.flash("error");
+    res.locals.currentUser=req.user;
+    next();
+})
+
+app.use("/login",loginRouter);
+app.use("/listing",listingRouter); 
+app.use("/signup",userRouter);
+app.use("/logout",logoutRouter);
+
 
 app.get("/",(req,res)=>{
     res.send("I am at root");
 })
+
+/* app.use("/demoUser",async (req,res)=>{
+    let fakeUser=new User({
+        email:"gyan@gmail.com",
+        username:"ironman",
+    })
+    let registerUser= await User.register(fakeUser,"sallubhai");
+    res.send(fakeUser);
+
+}) */
 
 app.get("/allListing",
     wrapAsync(async(req,res)=>{
@@ -43,75 +85,21 @@ app.get("/allListing",
     res.render("listing/index",{allListing});
 }))
 
-// adding new content
-app.get("/listing/new",(req,res)=>{
-    res.render("listing/addnew")
-})
 
-//route for adding new  listing......
-app.post("/listing",wrapAsync(async(req,res)=>{
-/*    try{
- */    const {title,description,filename,price,location,country}=req.body;
-    const newSample=new Listing({
-        title:title,
-        description:description,
-        image:{
-            filename:"",
-            url:filename,
-        },
-        price:price,
-        location:location,
-        country:country,
-    })
-    await newSample.save();
-    res.redirect("/allListing");
- /*   } catch(err){
-    next(err);
-   } */
-}))
-// show route
-app.get("/listing/:id",
-    wrapAsync(async(req,res)=>{
-    const {id}=req.params;
-    const showListing=await Listing.findById(id);
-    res.render("listing/show",{showListing});
-}))
-//Route for edit
-app.get("/listing/:id/edit",
-    wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    let newData=await Listing.findById(id);
-    res.render("listing/edit",{newData});
-}))
-//route for update data.... or listing
-app.put("/listing/:id",
-    wrapAsync(async(req,res)=>{
-    const {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{
-    title: req.body.title,
-    description: req.body.description,
-    image: { url: req.body.imageUrl },
-    price: req.body.price,
-    location: req.body.location,
-    country: req.body.country
-    })
-    res.redirect(`/listing/${id}`);
-}))
 
-//deleteing data....
-app.delete("/listing/:id",
-    wrapAsync(async(req,res)=>{
-    const {id}=req.params;
-    const deletedData=await Listing.findByIdAndDelete(id);
-    console.log(deletedData);
-    res.redirect("/allListing");
-}))
+
+
+
+
+
+
+
 
 
 
 app.use((err,req,res,next)=>{
     const {statusCode,message}=err;
-    res.status(statusCode).send(message);
+    res.status(404).send(message);
 })
 
 
